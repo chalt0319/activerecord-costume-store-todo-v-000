1c902188494628c3ca70a868c9e2fec8bf239bf7
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-19 18:52:52 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.text :location
       t.text :theme
       t.integer :price
-      t.boolean :family_friendly?
+      t.boolean :family_friendly
       t.integer :opening_date
       t.integer :closing_date
       t.text :long_description
