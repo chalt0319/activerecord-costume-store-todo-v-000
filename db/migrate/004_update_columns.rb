444deb106088a85f0eb1841f147078a9f502f424
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-19 18:59:41 +0000

    automatically backed up by learn

--- db/migrate/004_update_columns.rb
+++ db/migrate/004_update_columns.rb
@@ -1,8 +1,7 @@
 class UpdateColumns < ActiveRecord::Migration[5.1]
   def change
-    change_column :costume_stores, :column_name, :type
     rename_column :costume_stores, :number_of_employees, :num_of_employees
-    
+
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
