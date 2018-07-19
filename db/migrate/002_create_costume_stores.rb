554e6ed816726a8ce5b688d474c62c90e0f8eeda
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-19 18:51:41 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.text :name
       t.text :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
-      t.boolean :still_in_business?
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.integer :opening_time
       t.integer :closing_time
     end
